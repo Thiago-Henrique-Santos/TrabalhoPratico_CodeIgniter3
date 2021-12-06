0f47bf5da4e15984d777c1597f988f07f9eccebb
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-12-06 16:57:40 -0300

    Primeiro commit TP4BIM CodeIgniter3 com repositório remoto

--- BancoDadosTP_4/conexao_mysql.php
+++ BancoDadosTP_4/conexao_mysql.php
@@ -1,11 +1,15 @@
 ﻿<?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $banco = "BD_TP_4";
 
-    $conexao = new mysqli ($servidor, $usuario, $senha, $banco);
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "BD_TP_4";
 
-    if($conexao->connect_error)
-        die ("Conexão falhou: " . $conexao->connect_error);
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+if ($conexao->connect_error)
+    die("Conexão falhou: " . $conexao->connect_error);
+
 ?>
+
+//Iniciando conexao my_sql
